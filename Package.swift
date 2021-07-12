742472493060cf614baa854f13796caf98caa064
Ryan Morales <rymorale@adobe.com>
2021-07-12 12:46:51 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
   .library(name: "AEPTarget", targets: ["AEPTarget"]),
  ],
  dependencies: [
-  .package(url: "https://github.com/adobe/aepsdk-core-ios.git", .upToNextMajor(from: "3.0.0"))
+  .package(url: "https://github.com/adobe/aepsdk-core-ios.git", .upToNextMajor(from: "3.1.0"))
  ],
  targets: [
   .target(name: "AEPTarget",
