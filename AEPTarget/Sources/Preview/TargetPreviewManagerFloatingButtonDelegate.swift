0dfa5347e00124f97443a03174ee527c045c5570
Isaac Schmidt <ischmidt@nba.com>
2023-04-04 14:09:35 -0700

    Marks additional UI-specific classes as only available on iOS.

--- AEPTarget/Sources/Preview/TargetPreviewManagerFloatingButtonDelegate.swift
+++ AEPTarget/Sources/Preview/TargetPreviewManagerFloatingButtonDelegate.swift
@@ -9,7 +9,7 @@
  OF ANY KIND, either express or implied. See the License for the specific language
  governing permissions and limitations under the License.
  */
-
+#if os(iOS)
 import AEPServices
 
 ///
@@ -26,3 +26,4 @@ extension TargetPreviewManager: FloatingButtonDelegate {
 
     func onPanDetected() {}
 }
+#endif
