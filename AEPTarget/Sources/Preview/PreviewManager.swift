d3e3354689b0acb6eefe4c752014c47953b20ef1
Isaac Schmidt <ischmidt@nba.com>
2023-04-04 14:02:26 -0700

    Scope's `FullScreenPresentable` protocol to `iOS`.

--- AEPTarget/Sources/Preview/PreviewManager.swift
+++ AEPTarget/Sources/Preview/PreviewManager.swift
@@ -9,7 +9,7 @@
  OF ANY KIND, either express or implied. See the License for the specific language
  governing permissions and limitations under the License.
  */
-
+#if os(iOS)
 import AEPCore
 import AEPServices
 import Foundation
@@ -59,3 +59,4 @@ protocol PreviewManager {
     ///
     var previewToken: String? { get }
 }
+#endif
